24e3084d9feec353f63aebd68ed6a5ee4d9427b2
Flo Greistorfer <flo.greistorfer@sorogon.eu>
2026-07-08 07:30:12 +0200

    added automatic icon generation

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,12 +4,39 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const { GenerateSW } = require("workbox-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
+      meta: {
+        description: {
+          name: "description",
+          content: "Create and print custom Gaslands dashboard cards.",
+        },
+        "og:title": {
+          property: "og:title",
+          content: "Gaslands Dashboard Creator",
+        },
+        "og:type": {
+          property: "og:type",
+          content: "website",
+        },
+        "og:url": {
+          property: "og:url",
+          content: "https://gaslands.sorogon.eu/",
+        },
+        "og:image": {
+          property: "og:image",
+          content: "https://gaslands.sorogon.eu/og-image.png",
+        },
+        "og:image:alt": {
+          property: "og:image:alt",
+          content: "Gaslands Dashboard Creator",
+        },
+      },
     }),
     new CopyPlugin({
       patterns: [
@@ -23,11 +50,8 @@ module.exports = merge(common, {
         { from: "css", to: "css" },
         { from: "font", to: "font" },
         { from: "icon.svg", to: "icon.svg" },
-        { from: "favicon.ico", to: "favicon.ico" },
         { from: "robots.txt", to: "robots.txt" },
-        { from: "icon.png", to: "icon.png" },
         { from: "404.html", to: "404.html" },
-        { from: "site.webmanifest", to: "site.webmanifest" },
         { from: ".htaccess", to: ".htaccess", toType: "file" },
       ],
     }),
@@ -37,6 +61,18 @@ module.exports = merge(common, {
       cleanupOutdatedCaches: true,
       navigateFallback: "/index.html",
       exclude: [/\.htaccess$/],
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./icon.png",
+      cache: true,
+      favicons: {
+        appName: "Gaslands Dashboard Creator",
+        appDescription: "Create and print custom Gaslands dashboards.",
+        theme_color: "#eee",
+        background: "#111",
+        start_url: "/?utm_source=homescreen",
+        display: "standalone",
+      },
     }),
   ],
   optimization: {
